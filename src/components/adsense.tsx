c0b7ffb0c604b197892b1ba01d67f8306254c731
zaphodh <luispaulo.ni@gmail.com>
2025-09-09 12:26:48 -0300

    addede metatag for verification in google adsense

--- src/components/adsense.tsx
+++ src/components/adsense.tsx
@@ -2,11 +2,14 @@ import Script from "next/script";
 
 export default function Adsense() {
     return (
-        <Script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT!}`}
-            crossOrigin="anonymous"
-            strategy="lazyOnload"
-        ></Script>
+        <>
+            <Script
+                async
+                src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT!}`}
+                crossOrigin="anonymous"
+                strategy="lazyOnload"
+            ></Script>
+            <meta name="google-adsense-account" content={`${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT!}`}></meta>
+        </>
     );
 }
