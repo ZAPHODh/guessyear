65c3184bfed23c8e4b20a7d92e22d578b6895d2d
zaphodh <luispaulo.ni@gmail.com>
2025-10-17 11:18:22 -0300

    added new size to container

--- src/app/[locale]/(game)/lobby/[id]/page.tsx
+++ src/app/[locale]/(game)/lobby/[id]/page.tsx
@@ -49,7 +49,7 @@ export default async function LobbyPage({ params }: LobbyPageProps) {
   }
 
   return (
-    <div className="min-h-screen bg-background">
+    <div className="container mx-auto px-4 py-8 max-w-6xl">
       <LobbyErrorBoundary>
         <LobbyRoomProvider lobby={lobby} user={user} sessionId={sessionId}>
           <div className="flex flex-col min-h-screen">
